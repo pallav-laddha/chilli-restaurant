546493e994b398808d4b389cb208eadf259c398c
pallav laddha <pladdha12345@gmail.com>
2019-01-27 18:54:18 +0530

    single page application part1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { MenuComponent } from '../menu/menu.component';
-import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -9,5 +9,6 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'contactus',     component: ContactComponent }
 ];
