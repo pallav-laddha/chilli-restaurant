c57327a07eb29d83a5eba63e22b736bc4e9a77cd
pallav laddha <pladdha12345@gmail.com>
2019-01-27 21:54:48 +0530

    single page application part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'dishdetails/:id',     component: DishdetailsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'contactus',     component: ContactComponent }
 ];
