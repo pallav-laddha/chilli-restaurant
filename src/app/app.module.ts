26bf1e34deaa69e452e63480ffde2b8f20ec67dd
pallav laddha <pladdha12345@gmail.com>
2019-02-05 22:11:21 +0530

    dilog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,7 +10,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { DishService } from './services/dish.service';
+
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -21,7 +22,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
+import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import {LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
@@ -32,7 +37,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -43,9 +49,17 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     MatListModule
   ],
-    providers: [DishService],
+    providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
+    entryComponents: [
+       LoginComponent
+ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
